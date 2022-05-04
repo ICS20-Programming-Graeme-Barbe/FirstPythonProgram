303e2d1d09477cbf21d23fcafc18ab76b2017c2c
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-05-04 17:10:41 +0000

    index.python

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+# This is a pythagorean theorem calculator
+
 import math
 
-side_a = float(input("Enter side A length: "))
-side_b = float(input("Enter side B length: "))
-calc = side_a ** 2 + side_b ** 2
-square_root = math.sqrt(calc)
-rounded = round(square_root, 2)
+side_selector = input("Select side: ")
 
-print("C side is equal to ", rounded)23
+side_selector = side_selector.upper()
+
+
+if side_selector == "" and side_selector != "A" and side_selector != "B" and side_selector != "C":
+	print("You need to select what side you are trying to find.")
+	side_selector = input("Select side: ")
+	side_selector = side_selector.upper
+elif side_selector == "A":
+	side_b = float(input("Enter side B length: "))
+	side_c = float(input("Enter side C length: "))
+	units = input("Choose units:" )
+	calc = math.sqrt(side_c ** 2 - side_b ** 2)
+	rounded = round(calc, 2)
+	print("Side A is equal to ", rounded, units + "²")
+elif side_selector == "B":
+	side_a = float(input("Enter side A length: "))
+	side_c = float(input("Enter side C length: "))
+	calc = math.sqrt(side_c ** 2 - side_a ** 2)
+	rounded = round(calc, 2)
+	print("Side B is equal to ", rounded)
+elif side_selector == "C":
+	side_a = float(input("Enter side A length: "))
+	side_b = float(input("Enter side B length: "))
+	calc = math.sqrt(side_a ** 2 + side_b ** 2)
+	rounded = round(calc, 2)
+	print("Side C is equal to ", rounded)
+else:
+	print("Please choose between side A, B and C")
